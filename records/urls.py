29213378d3bfb427b3cae1dd785eebf382e94f6c
beyza <beyzamazici@gmail.com>
2026-06-26 22:44:18 +0300

    index.html sorgulama rotası düzeltildi

--- records/urls.py
+++ records/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('arac/', views.vehicle_detail, name='vehicle_detail'), # ID olmadan da buraya gelsin
-    path('arac/<int:vehicle_id>/', views.vehicle_detail, name='vehicle_detail_id'), # ID ile de çalışsın
+    path('arac/<int:vehicle_id>/', views.vehicle_detail, name='vehicle_detail'),
     path('bakim-ekle/', views.add_maintenance, name='add_maintenance'),
 ]
--- records/views.py
+++ records/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Vehicle
 from .forms import MaintenanceForm
 
-
 def index(request):
-    # Ana sayfa: Arama kutusundan gelen 'plaka' sorgusunu yakalar
     plaka = request.GET.get('plaka')
     error = None
 
@@ -17,18 +15,14 @@ def index(request):
 
     return render(request, 'index.html', {'error': error, 'plaka': plaka})
 
-
 def vehicle_detail(request, vehicle_id):
-    # Bu fonksiyon artık sadece ID ile çalışacak, çünkü plaka sorgusunu index'te çözdük
     vehicle = get_object_or_404(Vehicle, id=vehicle_id)
     maintenances = vehicle.maintenances.all().order_by('-date')
-
     context = {
         'vehicle': vehicle,
         'maintenances': maintenances,
     }
     return render(request, 'records/vehicle_detail.html', context)
-
 
 def add_maintenance(request):
     if request.method == 'POST':
@@ -38,5 +32,4 @@ def add_maintenance(request):
             return redirect('add_maintenance')
     else:
         form = MaintenanceForm()
-
     return render(request, 'records/add_maintenance.html', {'form': form})
