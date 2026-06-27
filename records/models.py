71cb8ce0a52ea7336c69c86930a33510f7b29a73
beyza <beyzamazici@gmail.com>
2026-06-27 19:50:50 +0300

    QR kod adresi ve optimizasyonu düzeltildi

--- records/models.py
+++ records/models.py
@@ -10,28 +10,33 @@ class Vehicle(models.Model):
     chassis_no = models.CharField(max_length=50, verbose_name="Şasi No")
     brand_model = models.CharField(max_length=100, verbose_name="Marka ve Model")
     vehicle_type = models.CharField(max_length=50, verbose_name="Tipi")
-    # YENİ EKLENEN ALAN: QR Kod Resmi
     qr_code = models.ImageField(upload_to='qr_codes/', blank=True, null=True, verbose_name="QR Kod")
 
     def __str__(self):
         return f"{self.plate} - {self.brand_model}"
 
-    # OTOMATİK QR KOD ÜRETME MOTORU
     def save(self, *args, **kwargs):
-        # Önce aracı veritabanına kaydedip bir ID alıyoruz
         super().save(*args, **kwargs)
 
-        # Eğer aracın henüz bir QR kodu yoksa şimdi oluştur
         if not self.qr_code:
-            # QR kodu okutunca gidilecek adres
-            qr_url = f"http://127.0.0.1:8000/arac/{self.id}/"
+            # KRİTİK DÜZELTME: Sunucunun gerçek adresi!
+            qr_url = f"https://otodijitaldefter.pythonanywhere.com/arac/{self.id}/"
 
-            qr_image = qrcode.make(qr_url)
+            # Daha sade ve okunabilir QR için ayarlar
+            qr = qrcode.QRCode(
+                version=1,
+                error_correction=qrcode.constants.ERROR_CORRECT_L,
+                box_size=10,
+                border=2,
+            )
+            qr.add_data(qr_url)
+            qr.make(fit=True)
+            qr_image = qr.make_image(fill_color="black", back_color="white")
+
             canvas = BytesIO()
             qr_image.save(canvas, format='PNG')
             canvas.seek(0)
 
-            # Resmi isimlendirip kaydet
             file_name = f'arac_{self.id}_qr.png'
             self.qr_code.save(file_name, File(canvas), save=False)
             super().save(*args, **kwargs)
